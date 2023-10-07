5da4cb13461f8bfb93eeb717dd0ed0551d987099
dat.truong <dat.truong@secomm.vn>
2023-10-07 23:25:36 +0700

    Module Scope

---
+++ local-modules/iife.js
@@ -0,0 +1,9 @@
+(function () {
+  const hero = "Batman";
+  console.log(hero);
+})();
+
+(function () {
+  const hero = "Superman";
+  console.log(hero);
+})();
--- local-modules/index.js
+++ local-modules/index.js
@@ -1,8 +1,2 @@
-const addFn = require("./add");
-
-console.log("Hello world from index");
-const sum = addFn(1, 3);
-const sum2 = addFn(4, 3);
-
-console.log(sum);
-console.log(sum2);
+require("./batman");
+require("./superman");
