57adc37af21fc25aace8da4022c414c148ae8073
dat.truong <dat.truong@secomm.vn>
2023-10-09 17:33:36 +0700

    ES Module

---
+++ local-modules/main.mjs
@@ -0,0 +1,23 @@
+// 1st 2nd
+// import add from "./math.mjs";
+// console.log(add(5, 5));
+
+// 3rd
+// import math from "./math.mjs";
+// console.log(math.add(2, 4));
+// console.log(math.subtract(12, 4));
+
+// // 4th
+// import math from "./math.mjs";
+// const { add, subtract } = math;
+// console.log(add(2, 4));
+// console.log(subtract(12, 4));
+
+// 5th
+// import * as math from "./math.mjs";
+// const { add, subtract } = math;
+
+// 6th
+import { add, subtract } from "./math.mjs";
+console.log(add(2, 4));
+console.log(subtract(12, 4));
---
+++ local-modules/math.mjs
@@ -0,0 +1,31 @@
+// // 1st
+// const add = (a, b) => {
+//   return a + b;
+// };
+
+// export default add;
+
+// // 2nd
+// export default (a, b) => {
+//   return a + b;
+// };
+
+// // 3rd
+// const add = (a, b) => {
+//   return a + b;
+// };
+// const subtract = (a, b) => {
+//   return a - b;
+// };
+// export default {
+//   add,
+//   subtract,
+// };
+
+// 4th
+export const add = (a, b) => {
+  return a + b;
+};
+export const subtract = (a, b) => {
+  return a - b;
+};
