1c19aa67b959e1d1daf504553b5fd69f31dba97b
dat.truong <dat.truong@secomm.vn>
2023-10-11 14:40:23 +0700

    ReadableStram and WritableStream

---
+++ built-in-modules/fs/stream.js
@@ -0,0 +1,13 @@
+const fs = require("node:fs");
+
+const readableStream = fs.createReadStream("./file.txt", {
+  encoding: "utf-8",
+  highWaterMark: 3,
+});
+
+const writeableStream = fs.createWriteStream("./file2.txt");
+
+readableStream.on("data", (chunk) => {
+  console.log(chunk);
+  writeableStream.write(chunk);
+});
