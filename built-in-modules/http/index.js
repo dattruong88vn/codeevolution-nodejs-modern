986634654d0872912e67f1b871b92e0f0ff4482a
dat.truong <dat.truong@secomm.vn>
2023-10-11 16:40:30 +0700

    http routing

--- built-in-modules/http/index.js
+++ built-in-modules/http/index.js
@@ -2,12 +2,27 @@ const http = require("node:http");
 const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
-  const name = "Thanh Dat";
-  let html = fs.readFileSync("./index.html", "utf-8");
-  html = html.replace("{{name}}", name);
+  const url = req.url;
+  switch (url) {
+    case "/":
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("Home page");
+      break;
+    case "/about":
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("About page");
+      break;
+    case "/api":
+      const data = { firstName: "Dat", lastName: "Truong" };
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(data));
+      break;
 
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end(html);
+    default:
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("Page Not Found");
+      break;
+  }
 });
 
 server.listen(8080, () => {
