93b7400d51226d7a1a22a24a4d8782ee9352f5ae
dat.truong <dat.truong@secomm.vn>
2023-10-11 16:31:25 +0700

    html template

--- built-in-modules/http/index.js
+++ built-in-modules/http/index.js
@@ -2,15 +2,12 @@ const http = require("node:http");
 const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
-  // use readFileSync for small file
-  //   const html = fs.readFileSync("./index.html");
+  const name = "Thanh Dat";
+  let html = fs.readFileSync("./index.html", "utf-8");
+  html = html.replace("{{name}}", name);
 
-  //   res.writeHead(200, { "Content-Type": "text/html" });
-  //   res.end(html);
-
-  // use stream for large file
-  const readableStream = fs.createReadStream(__dirname + "/index.html");
-  readableStream.pipe(res);
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end(html);
 });
 
 server.listen(8080, () => {
