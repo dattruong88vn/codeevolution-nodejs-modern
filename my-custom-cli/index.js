aa49c5098d59324b18746a6bed93fb24484850fb
dat.truong <dat.truong@secomm.vn>
2023-10-12 17:31:08 +0700

    building cli tools: parameter

--- my-custom-cli/index.js
+++ my-custom-cli/index.js
@@ -1,3 +1,15 @@
 #!/usr/bin/env node
+const yargs = require("yargs");
+const { argv } = yargs;
 
-console.log("Hello, my name is Dat");
+console.log(argv.pokemon);
+
+async function fetchMoves(pokemonName) {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+  );
+  const data = await response.json();
+  console.log(data.moves.slice(0, 5).map((item) => ({ name: item.move.name })));
+}
+
+fetchMoves(argv.pokemon);
