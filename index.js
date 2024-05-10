47a692377ffa134b99583e39ba8b1f8f74868300
dat.truong <dat.truong@secomm.vn>
2024-05-10 16:03:08 +0700

    allow cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end(
     JSON.stringify({
